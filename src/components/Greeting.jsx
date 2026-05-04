292ff403faa1e620b474a5a9e9f68c84b449e916
Liban Ibrahim Ali <libanibrahim812@gmail.com>
2026-05-04 09:39:05 +0300

    updated

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Greeting(props) {
+  return (
+  <h1>Hello, {props.name} {props.lastname}!</h1>
+  );
+}
+
+
+export default Greeting
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>This my first react</h1>
+    </div>
+  );
+}
+
+export default Header
